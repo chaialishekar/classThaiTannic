d569e7d750b55c2d3b9d1a62c217dac576a5545f
chaialishekar <77953625+chaialishekar@users.noreply.github.com>
2021-01-25 11:25:40 +0530

    Update ThaiTannicProj.cpp

--- ThaiTannicProj.cpp
+++ ThaiTannicProj.cpp
@@ -1,1 +1,12 @@
+#include<iostream>
+using namespace std;
+    class ThaiTannic{
+        public:
+        string name ,location,owners;
 
+        private:
+        int noOfStaff;
+        string staffName;
+    };
+    int main()
+ 
