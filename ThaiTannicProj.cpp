b1a55405d2d165cba32b9dfcd1b9d403813b3a4a
chaialishekar <77953625+chaialishekar@users.noreply.github.com>
2021-01-26 10:44:02 +0530

    Update ThaiTannicProj.cpp

--- ThaiTannicProj.cpp
+++ ThaiTannicProj.cpp
@@ -28,48 +28,51 @@ cout<<"\n-------------------------------------------------------"<<endl;
 cout<<"\n-------------------------------------------------------"<<endl;
 cout<<"\nSOUPS"<<endl;
 cout<<"\n-------------------------------------------------------"<<endl;   
-cout<<"\nVeg tom yum soup -110";
-cout<<"\nVeg man chow soup-110";
-cout<<"\nVeg wonton soup-110";
+cout<<"\n1)Veg tom yum soup -110";
+cout<<"\n2)Veg man chow soup-110";
+cout<<"\n3)Veg wonton soup-110";
 cout<<"\n-------------------------------------------------------"<<endl;
 cout<<"\nSTARTERS";
 cout<<"\n-------------------------------------------------------"<<endl;
-cout<<"\nDim sum(vegetable/paneer/chicken)-200";
-cout<<"\nPaneer Sichuan";
-cout<<"\nLotus stem gangjeong";
-cout<<"\nSpicy water-chestnut";
-cout<<"\nPotato fries";
-cout<<"\nCombos of two-any of the above";
+cout<<"\n4)Dim sum(vegetable/paneer/chicken)-200";
+cout<<"\n5)Paneer Sichuan-210";
+cout<<"\n6)Lotus stem gangjeong-250";
+cout<<"\n7)Spicy water-chestnut-190";
+cout<<"\n8)Potato fries-210";
+
 cout<<"\n-------------------------------------------------------"<<endl;
 cout<<"\nMAIN COURSE";
 cout<<"\n-------------------------------------------------------"<<endl;
 cout<<"\nVEG:";
-cout<<"\nThai green curry-220";
-cout<<"\nPad Thai-280";
-cout<<"\nSom tam-265";
-cout<<"\nVeg laska-160";
-cout<<"\nveg noodles-180";
+cout<<"\n9)Thai green curry-220";
+cout<<"\n10)Pad Thai-280";
+cout<<"\n11)Som tam-265";
+cout<<"\n12)Veg laska-160";
+cout<<"\n13)veg noodles-180";
 cout<<"\nNON VEG:";
-cout<<"\nChicken kimchi fried rice-280";
+cout<<"\n14)Chicken kimchi fried rice-280";
 cout<<"\n-------------------------------------------------------"<<endl;
 cout<<"\nMEALS:";
 cout<<"\n-------------------------------------------------------"<<endl;
-cout<<"\nPrawn ramen noodles-200";
-cout<<"\nAsian pot noodles-360";
-cout<<"\nThai wonton vegetable pot-380";
+cout<<"\n15)Prawn ramen noodles-200";
+cout<<"\n16)Asian pot noodles-360";
+cout<<"\n17)Thai wonton vegetable pot-380";
 cout<<"\n-------------------------------------------------------"<<endl;
 cout<<"\nDESERTS:";
 cout<<"\n-------------------------------------------------------"<<endl;
-cout<<"\nCoconut ice cream-140";
-cout<<"\nhai jelly-185";
-cout<<"\nMango sticky rice-185";
-cout<<"\nChocolate fudge ice-cream-160";
-cout<<"\nChocolate lava cake-130";
+cout<<"\n18)Coconut ice cream-140";
+cout<<"\n19)thai jelly-185";
+cout<<"\n20)Mango sticky rice-185";
+cout<<"\n21)Chocolate fudge ice-cream-160";
+cout<<"\n22)Chocolate lava cake-130";
+cout<<"\n23)Combos of two-any of the above-350";
 cout<<"\n-------------------------------------------------------"<<endl;
 }
       
            
 };
+
+dishes[23]={"1)Veg tom yum soup","2)Veg man chow soup","3)Veg wonton soup","4)Dim sum(vegetable/paneer/chicken)","5)Paneer Sichuan","6)Lotus stem gangjeong","7)Spicy water-chestnut","8)Potato fries","9)Thai green curry","10)Pad Thai","11)Som tam","12)Veg laska","13)veg noodles","14)Chicken kimchi fried rice","15)Prawn ramen noodles","16)Asian pot noodles","17)Thai wonton vegetable pot","18)Coconut ice cream","19)thai jelly","20)Mango sticky rice","21)Chocolate fudge ice-cream","22)Chocolate lava cake","23)a combo of any two dessert"}
 //class TableNo checks availability and allots available table
 
 class TableNo : public ThaiTannic 
